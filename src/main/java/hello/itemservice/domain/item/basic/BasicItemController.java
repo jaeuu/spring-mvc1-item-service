4280386852f7b20b829bb714f97998527075dd83
jaewoo Lim <130232709+jaeuu@users.noreply.github.com>
2023-08-16 00:33:43 +0900

    item controller

---
+++ src/main/java/hello/itemservice/domain/item/basic/BasicItemController.java
@@ -0,0 +1,34 @@
+package hello.itemservice.domain.item.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/basic/items")
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model){
+        List<Item> items = itemRepository.findByAll();
+        model.addAttribute("items",items);
+        return "basic/items";
+    }
+
+    @PostConstruct
+    public void init(){
+        itemRepository.save(new Item("testA",10000,10));
+        itemRepository.save(new Item("testB",20000,20));
+    }
+}
